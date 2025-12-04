6592170299819933e5db8c157b7ec9462e177f6f
JoemMastertech <mastertechnologyjoel@gmail.com>
2025-12-04 12:17:36 -0600

    feat: extract variables and themes to settings/variables.css

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -3,4 +3,22 @@ module.exports = async (page, scenario, vp) => {
   await require('./clickAndHoverHelper')(page, scenario);
 
   // add more ready handlers here...
+  await page.addStyleTag({
+    content: `
+      ::-webkit-scrollbar {
+        display: none;
+      }
+      body {
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
+        background: #000 !important;
+        background-image: none !important;
+      }
+      #background-video {
+        display: none !important;
+        opacity: 0 !important;
+        visibility: hidden !important;
+      }
+    `
+  });
 };
